cab8ddf20f06dfbb8d393ae65e4be0a7a9f9f65f
omelendrez <omar.melendrez@gmail.com>
2020-06-25 19:54:00 -0300

    Agregar barra de navegación

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import './components/Login'
 import Login from './components/Login'
+import NavBar from './components/NavBar'
 
 function App() {
   return (
-    <Login />
+    <>
+      <NavBar />
+      <Login />
+    </>
   )
 }
 
