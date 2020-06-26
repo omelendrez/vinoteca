7fd4ab2f322bcf0993aa8d2459e30f12344a53d5
omelendrez <omar.melendrez@gmail.com>
2020-06-26 20:15:48 -0300

    Optimizar menú de opciones

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,14 +9,10 @@ const NavBar = ({ user }) => {
   }
 
   return (
-    <nav className="navbar" role="navigation" aria-label="main navigation">
+    <nav className="navbar is-light" role="navigation" aria-label="main navigation">
       <div className="navbar-brand">
         <a className="navbar-item" href="https://bulma.io">
-          <img
-            src="https://bulma.io/images/bulma-logo.png"
-            width="112"
-            height="28"
-          />
+          La Cava
         </a>
 
         <a
@@ -36,11 +32,45 @@ const NavBar = ({ user }) => {
         {user.id &&
           <>
             <div className="navbar-start">
-              <a className="navbar-item">Productos</a>
-              <a className="navbar-item">Depósitos</a>
-              <a className="navbar-item">Categorías</a>
-              <a className="navbar-item">Inventario</a>
+
+              <div className="navbar-item has-dropdown is-hoverable">
+                <a className="navbar-link">Productos</a>
+                <div className="navbar-dropdown">
+                  <a className="navbar-item">Categorías</a>
+                  <a className="navbar-item">Productos</a>
+                </div>
+              </div>
+
+              <div className="navbar-item has-dropdown is-hoverable">
+                <a className="navbar-link">Inventario</a>
+                <div className="navbar-dropdown">
+                  <a className="navbar-item">Inventario</a>
+                  <a className="navbar-item">Faltantes</a>
+                  <a className="navbar-item">Correción de inventario</a>
+                  <a className="navbar-item">Motivos variación</a>
+                </div>
+              </div>
+
+              <div className="navbar-item has-dropdown is-hoverable">
+                <a className="navbar-link">Órdenes de compra</a>
+                <div className="navbar-dropdown">
+                  <a className="navbar-item">Proveedores</a>
+                  <a className="navbar-item">Órdenes de compra</a>
+                  <a className="navbar-item">Depósitos</a>
+                </div>
+              </div>
+
+              {user.profileId === 1 && <div className="navbar-item has-dropdown is-hoverable">
+                <a className="navbar-link">Usuarios</a>
+                <div className="navbar-dropdown">
+                  <a className="navbar-item">Empresas</a>
+                  <a className="navbar-item">Perfiles</a>
+                  <a className="navbar-item">Usuarios</a>
+                </div>
+              </div>}
+
             </div>
+
             <div className="navbar-end">
               <div className="navbar-item">
                 <span className="tag is-info">
