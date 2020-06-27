72d8e13e3b6f0f758374110cbdfc2cc751e68fc7
omelendrez <omar.melendrez@gmail.com>
2020-06-27 14:13:06 -0300

    Add token to api calls

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,14 @@
 import axios from 'axios'
+import { getData } from '../localStorage'
 
-export default axios.create({
+const api = axios.create({
   headers: {
     'Accept': 'application/json',
     'Content-Type': 'application/json'
   },
   baseURL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:3000/api/v1'
 })
+
+api.defaults.headers.common['Authorization'] = `Bearer ${getData('token')}`
+
+export default api
