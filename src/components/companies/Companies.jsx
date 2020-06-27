9c21f9eb8df6a9dd3ba5d4b670c4fccc01b469e5
omelendrez <omar.melendrez@gmail.com>
2020-06-27 18:26:25 -0300

    Optimizar para desktop

--- src/components/companies/Companies.jsx
+++ src/components/companies/Companies.jsx
@@ -30,45 +30,50 @@ const Companies = () => {
     <>
       {alert.message && <Notification message={alert.message} clear={clearAlert} type={alert.type} />}
       <section className="hero is-small is-light is-bold">
-        <div className="hero-body">
-          <div className="container">
-            <h1 className="title">
-              Empresas
+        <div className="columns is-centered">
+          <div className="column is-8">
+            <div className="hero-body">
+              <div className="container">
+                <h1 className="title">
+                  Empresas
             </h1>
-            <h2 className="subtitle">
-              Administración de empresas
+                <h2 className="subtitle">
+                  Administración de empresas
             </h2>
-          </div>
-          {rows &&
-            <section>
-              {rows.map((company, index) => {
-                const { name, contact, address, email, phone, created } = company
-                return (
-                  <div className="card my-3" key={index}>
-                    <header className="card-header has-background-link-dark">
-                      <p className="card-header-title  has-text-white">
-                        {name}
-                      </p>
-                    </header>
-                    <div className="card-content">
-                      <div className="content">
-                        <div><i className="fa fa-user mr-2"></i>{contact}</div>
-                        <div><i className="fa fa-map-marker-alt mr-2"></i>{address}</div>
-                        <div><i className="fa fa-at mr-2"></i>{email}</div>
-                        <div><i className="fa fa-phone mr-2"></i>{phone}</div>
-                        <br />
-                        <time><i className="fa fa-calendar-alt mr-2"></i>{formatDateFull(created)}</time>
+              </div>
+              {rows &&
+                <section>
+                  {rows.map((company, index) => {
+                    const { name, contact, address, email, phone, created } = company
+                    return (
+
+                      <div className="card my-3" key={index}>
+                        <header className="card-header has-background-link-dark">
+                          <p className="card-header-title  has-text-white">
+                            {name}
+                          </p>
+                        </header>
+                        <div className="card-content">
+                          <div className="content">
+                            <div><i className="fa fa-user mr-2"></i>{contact}</div>
+                            <div><i className="fa fa-map-marker-alt mr-2"></i>{address}</div>
+                            <div><i className="fa fa-at mr-2"></i>{email}</div>
+                            <div><i className="fa fa-phone mr-2"></i>{phone}</div>
+                            <br />
+                            <time><i className="fa fa-calendar-alt mr-2"></i>{formatDateFull(created)}</time>
+                          </div>
+                        </div>
+                        <footer className="card-footer">
+                          <a href="#" className="card-footer-item"><i className="fa fa-edit"></i></a>
+                          <a href="#" className="card-footer-item"><i className="fa fa-trash"></i></a>
+                        </footer>
                       </div>
-                    </div>
-                    <footer className="card-footer">
-                      <a href="#" className="card-footer-item"><i className="fa fa-edit"></i></a>
-                      <a href="#" className="card-footer-item"><i className="fa fa-trash"></i></a>
-                    </footer>
-                  </div>
-                )
-              }
-              )}
-            </section>}
+                    )
+                  }
+                  )}
+                </section>}
+            </div>
+          </div>
         </div>
       </section>
       {isLoading && <Loading />}
