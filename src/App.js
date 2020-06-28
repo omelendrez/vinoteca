58fa85a0503f10d7ccad3de5e943175e955386b3
frustratedcup <ciediva@gmail.com>
2020-06-28 20:07:21 -0300

    Resolve #10 Listado de depósitos y correción en companies

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React, { useState } from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import './components/Login'
-import Login from './components/Login'
-import NavBar from './components/NavBar'
+import React, { useState } from "react"
+import { BrowserRouter, Switch, Route } from "react-router-dom"
+import "./components/Login"
+import Login from "./components/Login"
+import NavBar from "./components/NavBar"
 
-import Home from './components/Home'
-import Companies from './components/companies/Companies'
-import Profiles from './components/profiles/Profiles'
-import Users from './components/users/Users'
+import Home from "./components/Home"
+import Companies from "./components/companies/Companies"
+import Profiles from "./components/profiles/Profiles"
+import Users from "./components/users/Users"
+import Stores from "./components/stores/Stores"
 
 function App() {
   const [user, setUser] = useState({})
@@ -16,15 +17,17 @@ function App() {
       <BrowserRouter>
         <NavBar user={user} setUser={setUser} />
         {!user.id && <Login setUser={setUser} />}
-        {user.id && <main>
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/companies" exact component={Companies} />
-            <Route path="/profiles" exact component={Profiles} />
-            <Route path="/users" exact component={Users} />
-          </Switch>
-        </main>
-        }
+        {user.id && (
+          <main>
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/companies" exact component={Companies} />
+              <Route path="/profiles" exact component={Profiles} />
+              <Route path="/users" exact component={Users} />
+              <Route path="/stores" exact component={Stores} />
+            </Switch>
+          </main>
+        )}
       </BrowserRouter>
     </>
   )
