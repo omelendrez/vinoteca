9237fdf6bfc97086a149c699043cc616cadfee8e
omelendrez <omar.melendrez@gmail.com>
2020-06-28 16:58:56 -0300

    Colorear íconos de edit y delete

--- src/components/common/TableItem.jsx
+++ src/components/common/TableItem.jsx
@@ -16,8 +16,8 @@ const TableItem = ({ item, itemHeader, children, handleEdit, handleDelete }) => 
       </div>
 
       <footer className="card-footer">
-        <a href="# " className="card-footer-item" onClick={e => handleEdit(e, item)}><i className="fa fa-edit"></i></a>
-        <a href="# " className="card-footer-item" onClick={e => handleDelete(e, item)}><i className="fa fa-trash"></i></a>
+        <a href="# " className="card-footer-item has-text-info" onClick={e => handleEdit(e, item)}><i className="fa fa-edit"></i></a>
+        <a href="# " className="card-footer-item has-text-danger" onClick={e => handleDelete(e, item)}><i className=" fa fa-trash-alt"></i></a>
       </footer>
     </div>
 
