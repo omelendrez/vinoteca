c5f8c0b1d6da29681127fc36c1354f25e12e88ca
matimancinelli <matiasmancinelli1998@hotmail.com>
2020-06-29 14:13:53 -0300

    Resolve #7

---
+++ src/services/products.js
@@ -0,0 +1,11 @@
+import api from "./api";
+import { handleError } from "../helpers";
+
+export const getProducts = () => {
+  return new Promise((resolve, reject) => {
+    api
+      .get("products")
+      .then((response) => resolve(response.data))
+      .catch((error) => reject(handleError(error)));
+  });
+};
