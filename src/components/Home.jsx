9028b9406efe62b62580312d32eaaa8e0bffc8ef
omelendrez <omar.melendrez@gmail.com>
2020-06-30 18:41:56 -0300

    Aplicar imagen de fondo en home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,6 @@
 import React from 'react'
+import './Home.scss'
 
-const Home = () => {
-  return (
-    <section className="hero is-primary">
-      <div className="hero-body">
-        <div className="container">
-          <h1 className="title">
-            La Cava
-        </h1>
-          <h2 className="subtitle">
-            Administración de stock
-        </h2>
-        </div>
-        <image src="../../logo1.jpg" alt="logo1" />
-      </div>
-    </section>
-  )
-}
+const Home = () => <div className="image" />
 
 export default Home
