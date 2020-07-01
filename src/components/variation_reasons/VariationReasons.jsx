94fe7d41127192751f97dc1f02d2c121d685722d
Ema <emarengo73@gmail.com>
2020-06-30 21:03:07 -0300

    resolve #58

--- src/components/variation_reasons/VariationReasons.jsx
+++ src/components/variation_reasons/VariationReasons.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react'
+import { Redirect } from 'react-router-dom'
 import Notification from '../common/Notification'
 import Loading from '../common/Loading'
 import Container from '../common/Container'
 import TableItem from '../common/TableItem'
 import TableItemField from '../common/TableItemField'
+import Confirm from '../common/Confirm'
 import { getVariationReasons, deleteVariationReason } from '../../services/variation_reasons'
 import { formatDateFull } from '../../helpers'
 
@@ -11,7 +13,9 @@ const VariationReasons = () => {
   const [variationReasons, setVariationReasons] = useState({ rows: [] })
   const [alert, setAlert] = useState({})
   const [isLoading, setIsLoading] = useState(false)
+  const [redirect, setRedirect] = useState('')
   const [update, setUpdate] = useState(false)
+  const [variationReason, setVariationReason] = useState({})
 
   useEffect(() => {
     setIsLoading(true)
@@ -32,19 +36,25 @@ const VariationReasons = () => {
 
   const handleEdit = (e, variationReason) => {
     e.preventDefault()
-    console.log(variationReason)
+    setRedirect({ pathname: '/edit-variation-reason', state: { variationReason } })
   }
 
   const handleDelete = async (e, variationReason) => {
     e.preventDefault()
+    setVariationReason(variationReason)
+  }
+
+  const confirmDelete = async () => {
     setIsLoading(true)
     await deleteVariationReason(variationReason)
+    setVariationReason({})
     setUpdate(!update)
   }
 
   const { rows } = variationReasons
   return (
     <>
+      {redirect && <Redirect to={redirect} />}
       {alert.message && <Notification message={alert.message} clear={clearAlert} type={alert.type} />}
 
       <Container
@@ -53,10 +63,19 @@ const VariationReasons = () => {
         width="is-6"
         background="is-primary"
       >
+        <button className="button" onClick={() => setRedirect('/add-variation-reason')}>
+          Agregar
+        </button>
         {rows && rows.map((variationReason, index) => {
           const { code, name, created } = variationReason
           return (
-            <TableItem key={index} item={variationReason} itemHeader={name} handleEdit={handleEdit} handleDelete={handleDelete}>
+            <TableItem
+              key={index}
+              item={variationReason}
+              itemHeader={name}
+              handleEdit={handleEdit}
+              handleDelete={handleDelete}
+            >
               <TableItemField label="Código" value={code} />
               <TableItemField label="Razón" value={name} />
               <br />
@@ -65,7 +84,13 @@ const VariationReasons = () => {
           )
         })
         }
-
+        <Confirm
+          title="Eliminando razón de variación"
+          message={<span>Confirma eliminación de la razón de variación <strong>{variationReason.name}</strong>?</span>}
+          handleOk={confirmDelete}
+          isActive={variationReason.id}
+          close={() => setVariationReason({})}
+        />
 
       </Container>
 
