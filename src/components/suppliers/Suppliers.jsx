ff57cecb5458ee04d9395faabb4d991278d8fd2d
Ema <emarengo73@gmail.com>
2020-06-30 21:18:23 -0300

    resolve #59

--- src/components/suppliers/Suppliers.jsx
+++ src/components/suppliers/Suppliers.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react'
+import { Redirect } from 'react-router-dom'
 import Notification from '../common/Notification'
 import Loading from '../common/Loading'
 import Container from '../common/Container'
 import TableItem from '../common/TableItem'
 import TableItemField from '../common/TableItemField'
+import Confirm from '../common/Confirm'
 import { getSuppliers, deleteSupplier } from '../../services/suppliers'
 import { formatDateFull } from '../../helpers'
 
@@ -11,7 +13,9 @@ const Suppliers = () => {
   const [suppliers, setSuppliers] = useState({ rows: [] })
   const [alert, setAlert] = useState({})
   const [isLoading, setIsLoading] = useState(false)
+  const [redirect, setRedirect] = useState('')
   const [update, setUpdate] = useState(false)
+  const [supplier, setSupplier] = useState({})
 
   useEffect(() => {
     setIsLoading(true)
@@ -24,7 +28,7 @@ const Suppliers = () => {
         setAlert({ message: error.message, type: 'is-danger' })
         setIsLoading(false)
       })
-  }, [])
+  }, [update])
 
   const clearAlert = () => {
     setAlert({})
@@ -32,31 +36,46 @@ const Suppliers = () => {
 
   const handleEdit = (e, supplier) => {
     e.preventDefault()
-    console.log(supplier)
+    setRedirect({ pathname: '/edit-supplier', state: { supplier } })
   }
 
   const handleDelete = async (e, supplier) => {
     e.preventDefault()
+    setSupplier(supplier)
+  }
+
+  const confirmDelete = async () => {
     setIsLoading(true)
     await deleteSupplier(supplier)
+    setSupplier({})
     setUpdate(!update)
   }
 
   const { rows } = suppliers
   return (
     <>
+      {redirect && <Redirect to={redirect} />}
       {alert.message && <Notification message={alert.message} clear={clearAlert} type={alert.type} />}
 
       <Container
         title="Proovedores"
-        subTitle="Admnistración de empresas"
+        subTitle="Admnistración de proveedores"
         width="is-6"
         background="is-primary"
       >
+        <button className="button" onClick={() => setRedirect('/add-supplier')}>
+          Agregar
+        </button>
         {rows && rows.map((supplier, index) => {
           const { name, contact, address, email, phone, created } = supplier
           return (
-            <TableItem key={index} item={supplier} itemHeader={name} handleEdit={handleEdit} handleDelete={handleDelete}>
+            <TableItem
+              key={index}
+              item={supplier}
+              itemHeader={name}
+              handleEdit={handleEdit}
+              handleDelete={handleDelete}
+            >
               <TableItemField icon="fa fa-user mr-2" value={contact} />
               <TableItemField icon="fa fa-map-marker-alt mr-2" value={address} />
               <TableItemField icon="fa fa-at mr-2" value={email} />
@@ -67,6 +86,14 @@ const Suppliers = () => {
           )
         })
         }
+        <Confirm
+          title="Eliminando proveedor"
+          message={<span>Confirma eliminación de proovedor <strong>{supplier.name}</strong>?</span>}
+          handleOk={confirmDelete}
+          isActive={supplier.id}
+          close={() => setSupplier({})}
+        />
+
       </Container>
 
       {!rows.length && <Notification message="La tabla no contiene registros" type="is-light" clear={clearAlert} />}
