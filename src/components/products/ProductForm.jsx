705b0e17e1632bd32a92a17cf4b65baec366b5ed
omelendrez <omar.melendrez@gmail.com>
2020-07-03 18:00:18 -0300

    Ocultar código al agregar campo

--- src/components/products/ProductForm.jsx
+++ src/components/products/ProductForm.jsx
@@ -1,15 +1,15 @@
-import React, { useState, useEffect } from "react";
-import { Redirect } from "react-router-dom";
-import Notification from "../common/Notification";
-import Loading from "../common/Loading";
-import Container from "../common/Container";
-import Form from "../common/Form";
-import FormField from "../common/FormField";
-import FormFieldSelect from "../common/FormFieldSelect";
-import { saveProduct, addProduct } from "../../services/products";
-import { getCategories } from "../../services/categories";
-import { cleanData } from "../../helpers";
-import { fields } from "./form.json";
+import React, { useState, useEffect } from "react"
+import { Redirect } from "react-router-dom"
+import Notification from "../common/Notification"
+import Loading from "../common/Loading"
+import Container from "../common/Container"
+import Form from "../common/Form"
+import FormField from "../common/FormField"
+import FormFieldSelect from "../common/FormFieldSelect"
+import { saveProduct, addProduct } from "../../services/products"
+import { getCategories } from "../../services/categories"
+import { cleanData } from "../../helpers"
+import { fields } from "./form.json"
 
 const ProductForm = (props) => {
   const formDefault = {
@@ -19,63 +19,63 @@ const ProductForm = (props) => {
     description: "",
     minimum: "",
     price: "",
-  };
+  }
 
-  const [form, setForm] = useState(formDefault);
-  const [redirect, setRedirect] = useState("");
-  const [alert, setAlert] = useState({});
-  const [isLoading, setIsLoading] = useState(false);
-  const [categories, setCategories] = useState([]);
+  const [form, setForm] = useState(formDefault)
+  const [redirect, setRedirect] = useState("")
+  const [alert, setAlert] = useState({})
+  const [isLoading, setIsLoading] = useState(false)
+  const [categories, setCategories] = useState([])
 
   useEffect(() => {
     if (props.location && props.location.state && props.location.state.product)
-      setForm(props.location.state.product);
-    getCategories().then((categories) => setCategories(categories.rows));
-  }, [props]);
+      setForm(props.location.state.product)
+    getCategories().then((categories) => setCategories(categories.rows))
+  }, [props])
 
   const clearAlert = () => {
-    setAlert({});
-  };
+    setAlert({})
+  }
 
   const handleChange = (e) => {
-    e.preventDefault();
+    e.preventDefault()
     setForm({
       ...form,
       [e.target.id]: e.target.value,
-    });
-  };
+    })
+  }
 
   const handleSave = (e) => {
-    e.preventDefault();
-    setIsLoading(true);
+    e.preventDefault()
+    setIsLoading(true)
     if (form.id) {
-      const cleanedForm = cleanData(form);
+      const cleanedForm = cleanData(form)
       saveProduct(cleanedForm)
         .then(() => {
-          setIsLoading(false);
-          setRedirect("/products");
+          setIsLoading(false)
+          setRedirect("/products")
         })
         .catch((error) => {
-          setIsLoading(false);
-          setAlert({ message: error.message, type: "is-danger" });
-        });
+          setIsLoading(false)
+          setAlert({ message: error.message, type: "is-danger" })
+        })
     } else {
       addProduct(form)
         .then(() => {
-          setIsLoading(false);
-          setRedirect("/products");
+          setIsLoading(false)
+          setRedirect("/products")
         })
         .catch((error) => {
-          setIsLoading(false);
-          setAlert({ message: error.message, type: "is-danger" });
-        });
+          setIsLoading(false)
+          setAlert({ message: error.message, type: "is-danger" })
+        })
     }
-  };
+  }
 
   const handleCancel = (e) => {
-    e.preventDefault();
-    setRedirect("/products");
-  };
+    e.preventDefault()
+    setRedirect("/products")
+  }
 
   return (
     <>
@@ -88,7 +88,7 @@ const ProductForm = (props) => {
         background="is-primary"
       >
         <Form
-          formHeader={form.id ? form.code : "Nuevo producto"}
+          formHeader={form.id ? form.name : "Nuevo producto"}
           handleSave={handleSave}
           handleCancel={handleCancel}
         >
@@ -104,20 +104,24 @@ const ProductForm = (props) => {
                 <option key={index} value={category.id}>
                   {category.name}
                 </option>
-              );
+              )
             })}
           </FormFieldSelect>
-          {fields.map((field, index) => (
-            <FormField
-              key={index}
-              label={field.label}
-              type={field.type}
-              fieldId={field.fieldId}
-              fieldValue={form[field.fieldId]}
-              handleChange={handleChange}
-              icon={field.icon}
-            />
-          ))}
+          {fields.map((field, index) => {
+            if (field.hideEmpty && !form[field.fieldId]) return null
+            return (
+              <FormField
+                key={index}
+                label={field.label}
+                type={field.type}
+                fieldId={field.fieldId}
+                fieldValue={form[field.fieldId]}
+                handleChange={handleChange}
+                icon={field.icon}
+                readOnly={field.readOnly}
+              />
+            )
+          })}
           {alert.message && (
             <Notification
               message={alert.message}
@@ -128,7 +132,7 @@ const ProductForm = (props) => {
         </Form>
       </Container>
     </>
-  );
-};
+  )
+}
 
-export default ProductForm;
+export default ProductForm
