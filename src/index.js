692ec3315e31f82a18cc50c57e8bfc5e55f5bee0
Omar Melendrez <omelendrez@bfr.com>
2020-07-08 12:24:05 -0500

    Usar fontawesome free kit

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 import 'bulma'
-import '@fortawesome/fontawesome-free/css/all.css'
-import '@fortawesome/fontawesome-free/js/all'
 
 ReactDOM.render(
   <React.StrictMode>
