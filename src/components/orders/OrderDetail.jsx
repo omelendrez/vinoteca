12d220ec93ef744c49c04de511141b755a916413
omelendrez <omar.melendrez@gmail.com>
2020-07-09 20:19:19 -0300

    Implementando detalle de órdenes

--- src/components/orders/OrderDetail.jsx
+++ src/components/orders/OrderDetail.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Item = ({ isSelected, item }) => {
+const Item = ({ isSelected, item, handleEdit, handleDelete }) => {
   const { productName, storeName, qtyRequested } = item
 
   return (
@@ -9,10 +9,10 @@ const Item = ({ isSelected, item }) => {
       <td>{storeName}</td>
       <th>{qtyRequested}</th>
       <td>
-        <button className="button is-info is-small"><i className="fa fa-edit"></i></button>
+        <button className="button is-info is-small" onClick={() => handleEdit(item)} ><i className="fa fa-edit"></i></button>
       </td>
       <td>
-        <button className="button is-danger is-small"><i className="fa fa-trash-alt"></i></button>
+        <button className="button is-danger is-small" onClick={() => handleDelete(item)} ><i className="fa fa-trash-alt"></i></button>
       </td>
     </tr>
   )
--- src/components/orders/OrderDetails.jsx
+++ src/components/orders/OrderDetails.jsx
@@ -1,7 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import Container from '../common/Container'
 import OrderDetail from './OrderDetail'
+import Modal from '../common/Modal'
+import Form from '../common/Form'
+import FormField from '../common/FormField'
+import FormFieldSelect from '../common/FormFieldSelect'
 import { getOrder } from '../../services/orders'
+import { getStores } from '../../services/stores'
+import { getProducts } from '../../services/products'
+import { fields } from './detailForm.json'
 
 const OrderDetails = (props) => {
   const detailsDefault = {
@@ -11,12 +18,42 @@ const OrderDetails = (props) => {
   }
   const [form, setForm] = useState(detailsDefault)
   const [order, setOrder] = useState({})
-  const [update, setUpdate] = useState(false)
+  const [showForm, setShowForm] = useState(false)
+  const [stores, setStores] = useState([])
+  const [products, setProducts] = useState([])
 
   useEffect(() => {
     getOrder(props.match.params.id)
       .then(order => setOrder(order))
-  }, [update])
+
+    getStores()
+      .then(stores => setStores(stores.rows))
+
+    getProducts()
+      .then(products => setProducts(products.rows))
+
+  }, [props.match.params.id])
+
+  const handleChange = e => {
+    setForm({ ...form, [e.target.id]: e.target.value })
+  }
+
+  const closeForm = () => {
+    setShowForm(false)
+  }
+
+  const handleOk = () => {
+    console.log(form)
+  }
+
+  const handleEdit = item => {
+    console.log(item)
+    setShowForm(true)
+  }
+
+  const handleDelete = item => {
+    console.log(item)
+  }
 
   const { orderDetails: items } = order
 
@@ -27,7 +64,7 @@ const OrderDetails = (props) => {
       width="is-8"
       background="is-warning">
 
-      <button className="button mx-1 my-1" onClick={() => { }}>
+      <button className="button mx-1 my-1" onClick={() => setShowForm(true)}>
         Agregar
         </button>
 
@@ -42,12 +79,66 @@ const OrderDetails = (props) => {
         </thead>
         <tbody>
           {items && items.map((item, index) => (
-            <OrderDetail key={index} item={item} />
+            <OrderDetail
+              key={index}
+              item={item}
+              handleEdit={handleEdit}
+              handleDelete={handleDelete}
+            />
           ))}
 
         </tbody>
 
       </table>
+
+      <Modal
+        isActive={showForm}
+      >
+        <Form
+          formHeader={form.id ? 'Editando detalle' : 'Agregando detalle'}
+          handleSave={handleOk}
+          handleCancel={closeForm}
+        >
+
+          {fields.map(field => {
+            if (field.hideEmpty && !form[field.fieldId]) return null
+            return (
+              <FormField
+                key={field.fieldId}
+                label={field.label}
+                type={field.type}
+                fieldId={field.fieldId}
+                fieldValue={form[field.fieldId]}
+                handleChange={handleChange}
+              />
+            )
+          })}
+
+          <FormFieldSelect
+            label="Depósito"
+            fieldId="storeId"
+            fieldValue={form.storeId}
+            handleChange={handleChange}
+          >
+            <option />
+            {stores.map(store => <option key={store.id} value={store.id}>{store.name}</option>)}
+
+          </FormFieldSelect>
+
+          <FormFieldSelect
+            label="Product"
+            fieldId="productId"
+            fieldValue={form.productId}
+            handleChange={handleChange}
+          >
+            <option />
+            {products.map(product => <option key={product.id} value={product.id}>{product.name}</option>)}
+
+          </FormFieldSelect>
+
+        </Form>
+
+      </Modal>
 
     </Container>
   )
