e4642131b7859659449fe87a4f7a5f4f3db73dfd
omelendrez <omar.melendrez@gmail.com>
2020-07-11 01:04:59 -0300

    Estilar botones para celulares en order details

--- src/components/orders/OrderDetail.jsx
+++ src/components/orders/OrderDetail.jsx
@@ -8,11 +8,11 @@ const Item = ({ isSelected, item, handleEdit, handleDelete }) => {
       <td>{productName}</td>
       <td>{storeName}</td>
       <th>{qtyRequested}</th>
-      <td>
-        <button className="button is-info is-small" onClick={() => handleEdit(item)} ><i className="fa fa-edit"></i></button>
+      <td className="px-0">
+        <button className="button is-white has-text-info" onClick={() => handleEdit(item)} ><i className="fa fa-edit"></i></button>
       </td>
-      <td>
-        <button className="button is-danger is-small" onClick={() => handleDelete(item)} ><i className="fa fa-trash-alt"></i></button>
+      <td className="px-2">
+        <button className="button is-white has-text-danger" onClick={() => handleDelete(item)} ><i className="fa fa-trash-alt"></i></button>
       </td>
     </tr>
   )
