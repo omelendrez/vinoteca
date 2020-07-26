60457d8cb696b8b520e406704e7fdae1b1b3aada
frustratedcup <ciediva@gmail.com>
2020-07-26 19:00:28 -0300

    Resolve #96

--- src/components/companies/Companies.jsx
+++ src/components/companies/Companies.jsx
@@ -3,11 +3,10 @@ import { Redirect } from 'react-router-dom'
 import Notification from '../common/Notification'
 import Loading from '../common/Loading'
 import Container from '../common/Container'
-import TableItem from '../common/TableItem'
-import TableItemField from '../common/TableItemField'
-import TableFooter from '../common/TableFooter'
+import List from '../common/List'
 import Confirm from '../common/Confirm'
 import { getCompanies, deleteCompany } from '../../services/companies'
+import { columns } from './list.json'
 
 const Companies = () => {
   const [companies, setCompanies] = useState({ rows: [] })
@@ -80,33 +79,12 @@ const Companies = () => {
 
         <Notification message={alert.message} className="mx-1 my-1" clear={clearAlert} type={alert.type} />
 
-        <div className="container list-container">
-          {rows && rows.map((company, index) => {
-            const { name, contact, address, email, phone, statusName, created, createdByName, updated, updatedByName } = company
-            return (
-              <TableItem
-                key={index}
-                item={company}
-                itemHeader={name}
-                handleEdit={handleEdit}
-                handleDelete={handleDelete}
-              >
-                <TableItemField icon="fa fa-user mr-2" value={contact} />
-                <TableItemField icon="fa fa-map-marker-alt mr-2" value={address} />
-                <TableItemField icon="fa fa-at mr-2" value={email} />
-                <TableItemField icon="fa fa-phone mr-2" value={phone} />
-                <TableFooter
-                  statusName={statusName}
-                  created={created}
-                  createdByName={createdByName}
-                  updated={updated}
-                  updatedByName={updatedByName}
-                />
-              </TableItem>
-            )
-          })
-          }
-        </div>
+        <List
+          rows={rows}
+          columns={columns}
+          handleEdit={handleEdit}
+          handleDelete={handleDelete}
+        />
 
         <Confirm
           title="Eliminando empresa"
