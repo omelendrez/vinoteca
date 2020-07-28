e785d827fed8e46e27a4229e1ef9147cf5766d01
matimancinelli <matiasmancinelli1998@hotmail.com>
2020-07-28 17:08:38 -0300

    Resolve  #104

--- src/components/inventory/LowStockProducts.jsx
+++ src/components/inventory/LowStockProducts.jsx
@@ -3,11 +3,9 @@ import { Redirect } from "react-router-dom"
 import Notification from "../common/Notification"
 import Loading from "../common/Loading"
 import Container from "../common/Container"
-import TableItem from "../common/TableItem"
-import TableItemField from "../common/TableItemField"
-import TableFooter from "../common/TableFooter"
+import List from '../common/List'
 import { getLowStockProducts } from "../../services/products"
-import { formatAmount } from "../../helpers"
+import { columns } from './list.json'
 
 const Products = () => {
   const [products, setProducts] = useState({ rows: [] })
@@ -52,42 +50,10 @@ const Products = () => {
 
         <Notification message={alert.message} className="mx-1 my-1" clear={clearAlert} type={alert.type} />
 
-        <div className="container list-container">
-
-          {rows && rows.map((product, index) => {
-            const { categoryName, name, code, description, lastPurchaseDate, lastPurchasePrice, lastSaleDate, lastSalePrice, minimum, quantity, price, statusName, created, createdByName, updated, updatedByName } = product
-            return (
-              <TableItem
-                key={index}
-                item={product}
-                itemHeader={name}
-              >
-                <Notification message={quantity <= minimum && quantity > 0 ? 'Producto con bajo stock' : ''} type="is-warning" clear={() => { }} />
-                <Notification message={quantity === 0 ? 'Producto sin stock' : ''} type="is-danger" clear={() => { }} />
-
-                <TableItemField label="Descripción" value={description} />
-                <TableItemField label="Código" value={code} />
-                <TableItemField label="Categoría" value={categoryName} />
-                <TableItemField label="Stock" value={quantity} />
-                <TableItemField label="Cantidad mínima" value={minimum} />
-                <TableItemField label="Precio" value={formatAmount(price)} />
-                <hr />
-                <TableItemField label="Última compra" value={lastPurchaseDate} />
-                <TableItemField label="Último precio de costo" value={`$ ${lastPurchasePrice.toFixed(2)}`} />
-                <TableItemField label="Última venta" value={lastSaleDate} />
-                <TableItemField label="Último precio de venta" value={`$ ${lastSalePrice.toFixed(2)}`} />
-                <TableFooter
-                  statusName={statusName}
-                  created={created}
-                  createdByName={createdByName}
-                  updated={updated}
-                  updatedByName={updatedByName}
-                />
-              </TableItem>
-            )
-          })
-          }
-        </div>
+        <List
+          rows={rows}
+          columns={columns}
+        />
 
         {isLoading && <Loading />}
 
