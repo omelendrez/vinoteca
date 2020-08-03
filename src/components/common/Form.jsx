4fe682f02a6964becca350ec7dbead00506f148b
omelendrez <omar.melendrez@gmail.com>
2020-08-03 19:51:33 -0300

    Implementar validación nombre, teléfono e email

--- src/components/common/Form.jsx
+++ src/components/common/Form.jsx
@@ -39,12 +39,40 @@ const Form = ({ formHeader, fields, currentForm, handleSave, handleCancel, error
     let errors = {}
     fields.map(field => {
       const fieldValue = form[field.fieldId]
+
       if (field.isRequired) {
         if (!fieldValue) {
           isOk = false
           errors = { ...errors, [field.fieldId]: ['is-danger', `${field.label} es un campo obligatorio`] }
         }
       }
+
+      if (field.isEmail) {
+        if (!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(fieldValue)) {
+          isOk = false
+          errors = { ...errors, [field.fieldId]: ['is-danger', `Dirección de ${field.label} no es válida`] }
+        }
+      }
+
+      if (field.isPhone) {
+        if (!/(?<=\s|:)\(?(?:(0?[1-3]\d{1,2})\)?(?:\s|-)?)?((?:\d[\d-]{5}|15[\s\d-]{7})\d+)/.test(fieldValue)) {
+          isOk = false
+          errors = { ...errors, [field.fieldId]: ['is-danger', `Número de ${field.label} no es válido (291 456456)`] }
+        }
+      }
+
+      if (field.isLimited) {
+        const [min, max] = field.isLimited
+        if (fieldValue.length < min) {
+          isOk = false
+          errors = { ...errors, [field.fieldId]: ['is-danger', `Campo ${field.label} tiene que tener al menos ${min} caracteres`] }
+        }
+        if (fieldValue.length > max) {
+          isOk = false
+          errors = { ...errors, [field.fieldId]: ['is-danger', `Campo ${field.label} no puede tener más de ${max} caracteres`] }
+        }
+      }
+
     })
     setFormErrors(errors)
     return isOk
