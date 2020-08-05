a05c3a3ab6f035d1589a758477d14a52d9f69014
omelendrez <omar.melendrez@gmail.com>
2020-08-05 19:41:41 -0300

    Abstractar validación en un file externo

--- src/components/common/Form.jsx
+++ src/components/common/Form.jsx
@@ -3,6 +3,7 @@ import FormField from './FormField'
 import FormTextArea from './FormTextArea'
 import FormFieldSelect from './FormFieldSelect'
 import Notification from '../common/Notification'
+import Validator from '../../validator'
 
 const Form = ({ formHeader, fields, currentForm, handleSave, handleCancel, error }) => {
   const [form, setForm] = useState(currentForm || {})
@@ -35,100 +36,8 @@ const Form = ({ formHeader, fields, currentForm, handleSave, handleCancel, error
   }
 
   const validate = () => {
-    let isOk = true
-    let errors = {}
-    fields.map(field => {
-      const fieldValue = form[field.fieldId]
-
-      if (field.isRequired) {
-        if (!fieldValue) {
-          isOk = false
-          errors = { ...errors, [field.fieldId]: ['is-danger', `${field.label} es un campo obligatorio`] }
-        }
-      }
-
-      if (field.isEmail) {
-        if (!/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(fieldValue)) {
-          isOk = false
-          errors = { ...errors, [field.fieldId]: ['is-danger', `Dirección de ${field.label} no es válida`] }
-        }
-      }
-
-      if (field.isPhone) {
-        if (!/(?<=\s|:)\(?(?:(0?[1-3]\d{1,2})\)?(?:\s|-)?)?((?:\d[\d-]{5}|15[\s\d-]{7})\d+)/.test(fieldValue)) {
-          isOk = false
-          errors = { ...errors, [field.fieldId]: ['is-danger', `Número de ${field.label} no es válido (291 456456)`] }
-        }
-      }
-
-      if (field.isLimited) {
-        const [min, max] = field.isLimited
-        if (fieldValue.length < min) {
-          isOk = false
-          errors = { ...errors, [field.fieldId]: ['is-danger', `Campo ${field.label} tiene que tener al menos ${min} caracteres`] }
-        }
-        if (fieldValue.length > max) {
-          isOk = false
-          errors = { ...errors, [field.fieldId]: ['is-danger', `Campo ${field.label} no puede tener más de ${max} caracteres`] }
-        }
-      }
-
-      if (field.onlyText) {
-        if (!/^[a-zA-Z ]*$/.test(fieldValue)) {
-          isOk = false
-          errors = { ...errors, [field.fieldId]: ['is-danger', `${field.label} no es válido, sólo se aceptan letras y espacios`] }
-        }
-      }
-
-      if (field.isRange) {
-        const [min, max] = field.isRange
-        if (fieldValue < min) {
-          isOk = false
-          errors = { ...errors, [field.fieldId]: ['is-danger', `Campo ${field.label} tiene que ser al menos ${min}`] }
-        }
-        if (max && fieldValue > max) {
-          isOk = false
-          errors = { ...errors, [field.fieldId]: ['is-danger', `Campo ${field.label} no puede ser más de ${max}`] }
-        }
-      }
-
-      if (field.compare) {
-        const [fld, sign] = field.compare
-        const secFldValue = form[fld]
-        switch (sign) {
-          case '<=':
-            if (!(fieldValue <= secFldValue)) {
-              isOk = false
-              errors = { ...errors, [field.fieldId]: ['is-danger', `${field.label} no puede ser mayor a ${secFldValue}`] }
-            }
-            break
-          case '>=':
-            if (!(fieldValue >= secFldValue)) {
-              isOk = false
-              errors = { ...errors, [field.fieldId]: ['is-danger', `${field.label} no puede ser menor a ${secFldValue}`] }
-            }
-            break
-          case '>':
-            if (!(fieldValue > secFldValue)) {
-              isOk = false
-              errors = { ...errors, [field.fieldId]: ['is-danger', `${field.label} debe ser mayor a ${secFldValue}`] }
-            }
-            break
-          case '<':
-            if (!(fieldValue < secFldValue)) {
-              isOk = false
-              errors = { ...errors, [field.fieldId]: ['is-danger', `${field.label} debe ser menor a ${secFldValue}`] }
-            }
-            break
-          default:
-            if (!(fieldValue = secFldValue)) {
-              isOk = false
-              errors = { ...errors, [field.fieldId]: ['is-danger', `${field.label} debe ser ${secFldValue}`] }
-            }
-        }
-      }
-    })
-
+    const [errors, isOk] = Validator(form, fields)
+    console.log(errors)
     setFormErrors(errors)
     return isOk
   }
---
+++ src/validator.js
@@ -0,0 +1,99 @@
+export default (form, fields) => {
+
+  let isOk = true
+  let errors = {}
+  fields.map(field => {
+    const fieldValue = form[field.fieldId]
+
+    if (field.isRequired) {
+      if (!fieldValue) {
+        isOk = false
+        errors = { ...errors, [field.fieldId]: ['is-danger', `${field.label} es un campo obligatorio`] }
+      }
+    }
+
+    if (field.isEmail) {
+      if (!/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(fieldValue)) {
+        isOk = false
+        errors = { ...errors, [field.fieldId]: ['is-danger', `Dirección de ${field.label} no es válida`] }
+      }
+    }
+
+    if (field.isPhone) {
+      if (!/(?<=\s|:)\(?(?:(0?[1-3]\d{1,2})\)?(?:\s|-)?)?((?:\d[\d-]{5}|15[\s\d-]{7})\d+)/.test(fieldValue)) {
+        isOk = false
+        errors = { ...errors, [field.fieldId]: ['is-danger', `Número de ${field.label} no es válido (291 456456)`] }
+      }
+    }
+
+    if (field.isLimited) {
+      const [min, max] = field.isLimited
+      if (fieldValue.length < min) {
+        isOk = false
+        errors = { ...errors, [field.fieldId]: ['is-danger', `Campo ${field.label} tiene que tener al menos ${min} caracteres`] }
+      }
+      if (fieldValue.length > max) {
+        isOk = false
+        errors = { ...errors, [field.fieldId]: ['is-danger', `Campo ${field.label} no puede tener más de ${max} caracteres`] }
+      }
+    }
+
+    if (field.onlyText) {
+      if (!/^[a-zA-Z ]*$/.test(fieldValue)) {
+        isOk = false
+        errors = { ...errors, [field.fieldId]: ['is-danger', `${field.label} no es válido, sólo se aceptan letras y espacios`] }
+      }
+    }
+
+    if (field.isRange) {
+      const [min, max] = field.isRange
+      if (fieldValue < min) {
+        isOk = false
+        errors = { ...errors, [field.fieldId]: ['is-danger', `Campo ${field.label} tiene que ser al menos ${min}`] }
+      }
+      if (max && fieldValue > max) {
+        isOk = false
+        errors = { ...errors, [field.fieldId]: ['is-danger', `Campo ${field.label} no puede ser más de ${max}`] }
+      }
+    }
+
+    if (field.compare) {
+      const [fld, sign] = field.compare
+      const secFldValue = form[fld]
+      switch (sign) {
+        case '<=':
+          if (!(fieldValue <= secFldValue)) {
+            isOk = false
+            errors = { ...errors, [field.fieldId]: ['is-danger', `${field.label} no puede ser mayor a ${secFldValue}`] }
+          }
+          break
+        case '>=':
+          if (!(fieldValue >= secFldValue)) {
+            isOk = false
+            errors = { ...errors, [field.fieldId]: ['is-danger', `${field.label} no puede ser menor a ${secFldValue}`] }
+          }
+          break
+        case '>':
+          if (!(fieldValue > secFldValue)) {
+            isOk = false
+            errors = { ...errors, [field.fieldId]: ['is-danger', `${field.label} debe ser mayor a ${secFldValue}`] }
+          }
+          break
+        case '<':
+          if (!(fieldValue < secFldValue)) {
+            isOk = false
+            errors = { ...errors, [field.fieldId]: ['is-danger', `${field.label} debe ser menor a ${secFldValue}`] }
+          }
+          break
+        default:
+          if (!(fieldValue = secFldValue)) {
+            isOk = false
+            errors = { ...errors, [field.fieldId]: ['is-danger', `${field.label} debe ser ${secFldValue}`] }
+          }
+      }
+    }
+  })
+
+  return [errors, isOk]
+
+}
