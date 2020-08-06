396a70cce731af96c48d26aa6b5b0faece0a09b4
Ema <emarengo73@gmail.com>
2020-08-06 19:38:59 -0300

    resolve #128

--- src/components/inventory/LowStockProducts.jsx
+++ src/components/inventory/LowStockProducts.jsx
@@ -5,7 +5,7 @@ import Loading from "../common/Loading"
 import Container from "../common/Container"
 import List from '../common/List'
 import { getLowStockProducts } from "../../services/products"
-import { columns } from './list.json'
+import { columns } from './lowStockList.json'
 
 const Products = () => {
   const [products, setProducts] = useState({ rows: [] })
@@ -39,14 +39,11 @@ const Products = () => {
       {redirect && <Redirect to={redirect} />}
 
       <Container
-        title="Inventario"
+        title="Faltantes"
         subTitle="Listado de productos faltantes"
         width="is-6"
         background="is-info"
       >
-        <button className="button mx-1 my-1" onClick={() => setRedirect("/add-order")}>
-          Crear orden de compra
-        </button>
 
         <Notification message={alert.message} className="mx-1 my-1" clear={clearAlert} type={alert.type} />
 
