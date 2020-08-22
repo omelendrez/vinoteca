afb8913d7f3e60ff37c57392f9516b21d4799f53
omelendrez <omar.melendrez@gmail.com>
2020-08-22 18:21:21 -0300

    Agregar botón de escanear en home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Scanner from './common/BarcodeScanner'
 import './Home.scss'
 
-const Home = () => <div className="image" />
+const Home = () => {
+  const [showScanner, setShowScanner] = useState(false)
+
+  const handleScan = e => {
+    e.preventDefault()
+    setShowScanner(true)
+  }
+
+  const handleCodeRead = code => {
+    if (code) {
+      console.log(code)
+    }
+    setShowScanner(false)
+  }
+
+  return (
+    <>
+      <div className="image" />
+      <button className="button btn-scan" onClick={e => handleScan(e)}>
+        <i className="fa fa-barcode fa-3x"></i>
+      </button>
+      <Scanner
+        show={showScanner}
+        codeRead={handleCodeRead}
+      />
+    </>
+  )
+}
 
 export default Home
